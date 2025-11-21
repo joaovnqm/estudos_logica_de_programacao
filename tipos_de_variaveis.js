4af9aa7a589c907d325bc0f2afa2687db067bc40
João Victor Macêdo <joaovictornqm@gmail.com>
2025-11-21 18:55:22 -0300

    Add initial attributes for Pokémon in tipos_de_variaveis.js

---
+++ tipos_de_variaveis.js
@@ -0,0 +1,6 @@
+//Atributos dados aos pokemons da imagem_1 do repositório.
+let nomePokemon = "Pikachu"
+let sexoPokemon = "Masculino"
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+let selecionavelPokemon = true
