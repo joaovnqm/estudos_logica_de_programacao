93b35d94b9369d59c460125683d55ed562a659ac
João Victor Macêdo <joaovictornqm@gmail.com>
2025-11-21 19:11:25 -0300

    Adicionando atributos do meu celular

--- tipos_de_variaveis.js
+++ tipos_de_variaveis.js
@@ -4,3 +4,13 @@ let sexoPokemon = "Masculino"
 let nivelPokemon = 20
 let pontosDeVidaPokemon = 45
 let selecionavelPokemon = true
+
+//Atribuindo valores ao meu telefone. (Memória RAM em GB/Capacidade em GB/Tamanho da tela em polegadas)
+let marcaTelefone = "Apple"
+let modeloTelefone = "iPhone 16e"
+let processadorTelefone = "A18 Bionic"
+let memoriaRamTelefone = 8
+let capacidadeTelefone = 128
+let cartaoDeMemoriaTelefone = false
+let numeroTelefone = "8199999-9999"
+let tamanhoTelaTelefone = 6.1
